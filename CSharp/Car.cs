23f6450abb53b3eab4786f06119eac88dceee2e0
aiden.peno <aiden.peno@d7c7096d-40ae-02fc-9a42-5b0c96bbf49d>
2010-08-18 09:35:49 +0000

    Second attempt at C# Version. Working GUI (without cars). Really only needs Program.cs to be updated (equivalent to java Simulation.java). Once that is going, cars should render straight away (code exists for that already)

---
+++ CSharp/Car.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficLightSim2
+{
+    public class Car
+    {
+        private int lanePosition;
+        private bool stopped;
+
+        public Car(int lanePosition)
+        {
+            this.lanePosition = lanePosition;
+        }
+
+        public bool intersects(Car car)
+        {
+            if (((car.getLanePosition() < (this.getLanePosition() + Settings.CAR_LENGTH)) && ((this.getLanePosition() + Settings.CAR_LENGTH) < (car.getLanePosition() + Settings.CAR_LENGTH)) || ((car.getLanePosition() < this.getLanePosition()) && (this.getLanePosition() < (car.getLanePosition() + Settings.CAR_LENGTH)))))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool intersects(int lanePosition, bool direction)
+        {
+            if (((this.getLanePosition() == lanePosition) && (direction == Settings.TRAFFIC_WEST_NORTH)) || ((lanePosition == (this.getLanePosition()+Settings.CAR_LENGTH) && (direction == Settings.TRAFFIC_EAST_SOUTH))))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int getLanePosition()
+        {
+            return this.lanePosition;
+        }
+
+        public void setLanePostion(int lanePosition)
+        {
+            this.lanePosition = lanePosition;
+        }
+
+        public void setStopped(bool stopped)
+        {
+            this.stopped = stopped;
+        }
+
+        public bool getStopped()
+        {
+            return this.stopped;
+        }
+
+        public void moveCar(int units)
+        {
+            lanePosition += units;
+        }
+    }
+}
