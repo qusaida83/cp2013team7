ca4ba25abccd4f60b969f88f5da63d52cefe478a
aiden.peno <aiden.peno@d7c7096d-40ae-02fc-9a42-5b0c96bbf49d>
2010-08-19 01:47:55 +0000

    Latest CSharp. To do: Car rendering and simulation.

---
+++ CSharp/TrafficLightSim2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TrafficLightSim2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Intersection ourIntersection = new Intersection();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TrafficLightSimulator());
+        }
+    }
+}
