23e7451c7eb92deac45b3ea656d15ac8d2182205
aiden.peno <aiden.peno@d7c7096d-40ae-02fc-9a42-5b0c96bbf49d>
2010-08-25 03:23:39 +0000

    Final version of CSharp. Just a couple of issues with changing lanes to fix.

---
+++ CSharp/Final Version/TrafficLightSim2/setVLanes.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TrafficLightSim2
+{
+    public partial class setVLanes : Form
+    {
+        mainWindow mainForm;
+
+        public setVLanes(mainWindow mainForm)
+        {
+            this.mainForm = mainForm;
+            InitializeComponent();
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            string Str = textBox1.Text.Trim();
+            short Num;
+            bool isNum = short.TryParse(Str, out Num);
+
+            if (isNum)
+            {
+                short num = Convert.ToInt16(textBox1.Text);
+                if (num < 4 && num > 0)
+                {
+                    Settings.getSimSettings().setVLanes(num);
+                    mainForm.Refresh();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Number");
+                }
+            }
+
+            else
+            {
+                MessageBox.Show("Enter number!");
+            }
+        }
+    }
+}
