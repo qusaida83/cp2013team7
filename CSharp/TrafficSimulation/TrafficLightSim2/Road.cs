86bc7717b3b4384623280e80e249f8e72353aa23
ashley.sexton89 <ashley.sexton89@d7c7096d-40ae-02fc-9a42-5b0c96bbf49d>
2010-10-12 12:27:42 +0000

    Automatic light changing implemented. Changing lane numbers in directions also working. Want to fix flickering in C# version. Also still spawning in same lanes for vertical, timing from orange to red may also be off. Cars still not moving.

---
+++ CSharp/TrafficSimulation/TrafficLightSim2/Road.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TrafficLightSim2
+{
+    public class Road
+    {
+        private List<Lane> eastSouthLanes = new List<Lane>();
+        private List<Lane> westNorthLanes = new List<Lane>();
+        private int roadLength;
+        private int roadSpeed;
+
+        public Road(short noEastSouthLanes, short noWestNorthLanes, int roadLength)
+        {
+            for (int i = 0; i < noEastSouthLanes; i++)
+            {
+                this.addLane(new Lane(), Settings.TRAFFIC_EAST_SOUTH);
+            }
+            for (int i = 0; i < noWestNorthLanes; i++)
+            {
+                this.addLane(new Lane(), Settings.TRAFFIC_WEST_NORTH);
+            }
+            this.roadLength = roadLength;
+        }
+
+     /*   public Road(short noLanes, int roadLength)
+        {
+            for (int i = 0; i < noLanes; i++)
+            {
+                addLane(new Lane());
+            }
+
+            trafficDirection = Settings.TRAFFIC_EAST_SOUTH;
+            this.roadLength = roadLength;
+        }*/
+        public List<Lane> getNeighbouringLanes(Lane lane, bool trafficDirection)
+        {
+            List<Lane> neighbours = new List<Lane>();
+            if ((this.getLanes(trafficDirection).IndexOf(lane) - 1) >= 0)
+            {
+                neighbours.Add(this.getLanes(trafficDirection)[this.getLanes(trafficDirection).IndexOf(lane) - 1]);
+            }
+            if ((this.getLanes(trafficDirection).IndexOf(lane) + 1 < this.getLanes(trafficDirection).Count()))
+            {
+                neighbours.Add(this.getLanes(trafficDirection)[this.getLanes(trafficDirection).IndexOf(lane) + 1]);
+            }
+            return neighbours;
+        }
+
+ /*       public List<Lane> getNeighbouringLanes(Lane lane)
+        {
+            List<Lane> neighbours = new List<Lane>();
+
+            if ((this.getLanes().IndexOf(lane) - 1) > 0)
+            {
+                neighbours.Add(this.getLanes()[this.getLanes().IndexOf(lane) - 1]);
+            }
+            if ((this.getLanes().IndexOf(lane) + 1 < this.getLanes().Count()))
+            {
+                neighbours.Add(this.getLanes()[this.getLanes().IndexOf(lane) + 1]);
+            }
+
+            return neighbours;
+        }*/
+
+        
+         // Add a lane to this road
+        
+        public void addLane(Lane lane, bool trafficDirection) 
+        {
+            getLanes(trafficDirection).Add(lane);
+        }
+
+        public void removeLane(bool trafficDirection) 
+        {
+            getLanes(trafficDirection).RemoveAt((this.getLanes(trafficDirection).Count()-1));
+        }
+ /*       public void removeLane()
+        {
+            getLanes().RemoveAt((this.getLanes().Count() - 1));
+        }
+ */
+         // Get the direction the traffic is moving.
+    /*     
+        public bool getTrafficDirection()
+        {
+            return trafficDirection;
+        }
+        */
+      
+         //Set the direction the traffic is moving.
+                  
+     /*   public void setTrafficDirection(bool trafficDirection)
+        {
+            this.trafficDirection = trafficDirection;
+        }*/
+
+         //Get the length of this road.         
+         
+        public int getRoadLength()
+        {
+            return roadLength;
+        }
+
+         //Set the length of this road.
+         
+        public void setRoadLength(int roadLength)
+        {
+            this.roadLength = roadLength;
+        }
+
+        public void trafficChangeLane(Lane l, Car c, Lane nl)
+        {
+            Car tempCar = c;
+            l.removeCar(c);
+            nl.addCar(tempCar);
+        }
+
+        //return the lanes
+
+        public List<Lane> getLanes(bool trafficDirection)
+        {
+            if (trafficDirection == Settings.TRAFFIC_EAST_SOUTH)
+            {
+                return eastSouthLanes;
+            }
+            else
+            {
+                return westNorthLanes;
+            }
+        }
+
+        public int getNoLanes(bool trafficDirection)
+        {
+            return getLanes(trafficDirection).Count();
+        }
+
+        public Lane getLane(bool trafficDirection, int laneNo)
+        {
+            return getLanes(trafficDirection)[laneNo];
+        }
+
+        public int getRoadSpeed()
+        {
+            return roadSpeed;
+        }
+
+        public void setRoadSpeed(int roadSpeed)
+        {
+            this.roadSpeed = roadSpeed;
+        }
+    }
+}
---
+++ CSharp/TrafficSimulation/TrafficLightSim2/setHLanes.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace TrafficLightSim2
+{
+    partial class setHLanes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+       protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.okButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Set East Direction";
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(137, 82);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 1;
+            this.okButton.Text = "Okay";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Set West Direction";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // setHLanes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 117);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.label1);
+            this.Name = "setHLanes";
+            this.Text = "Set Horizontal Lanes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
