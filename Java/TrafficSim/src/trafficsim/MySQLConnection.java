710f111d0e1bd86dd73b5c38ae62e1296cac1391
twistie.man <twistie.man@d7c7096d-40ae-02fc-9a42-5b0c96bbf49d>
2010-10-21 14:43:57 +0000

    bug fixes with GUI, start of MySQL connectivity.

---
+++ Java/TrafficSim/src/trafficsim/MySQLConnection.java
@@ -0,0 +1,69 @@
+package trafficsim;
+
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class which neatly handles all the MySQL Transactions, Configurations and Settings for MySQL Connectivity.
+ *
+ * @author Tristan Davey
+ *
+ */
+class MySQLConnection implements Serializable {
+
+    private String hostname;
+    private String username;
+    private String password;
+    private String database;
+    private Connection conn;
+
+    void MySQLConnection(String hostname, String username, String password, String database) {
+        this.hostname = hostname;
+        this.username = username;
+        this.password = password;
+        this.database = database;
+    }
+
+    public void openConnection() throws InstantiationException, IllegalAccessException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MySQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            this.conn = DriverManager.getConnection("jdbc:mysql://" + this.hostname + "/" + this.database + "?" + "user=" + this.username + "&password=" + this.password);
+        } catch (SQLException ex) {
+            Logger.getLogger(MySQLConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public ResultSet selectQuery(String queryString) {
+      try {
+        Statement statement = conn.createStatement();
+        ResultSet result = statement.executeQuery(queryString);
+        return result;
+      }
+      catch (SQLException s){
+        System.out.println(s.getMessage());
+        return null;
+      }
+    }
+
+    public void insertQuery(String queryString) {
+        try{
+            Statement st = conn.createStatement();
+            int val = st.executeUpdate(queryString);
+        }
+        catch (SQLException s){
+            System.out.println(s.getMessage());
+        }
+    }
+
+}
